18fc67a0f25cb65aae652c9fac23e886e035457a
Lucas Bigelow <lukejbigelow@gmail.com>
2020-07-08 11:40:55 -0500

    added a single lil question obj and array

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-/**
- * Example store structure
- */
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
+'use strict';
+
+// const store = {
+//   // 5 or more questions are required
+//   questions: [
+//     {
+//       question: 'What color is broccoli?',
+//       answers: [
+//         'red',
+//         'orange',
+//         'pink',
+//         'green'
+//       ],
+//       correctAnswer: 'green'
+//     },
+//     {
+//       question: 'What is the current year?',
+//       answers: [
+//         '1970',
+//         '2015',
+//         '2019',
+//         '2005'
+//       ],
+//       correctAnswer: '2019'
+//     }
+//   ],
+//   quizStarted: false,
+//   questionNumber: 0,
+//   score: 0
+// };
+
+const questions = [
+  {
+    question: 'what is your favorite color',
+    responses: ['red', 'blue', 'yellow', 'black'],
+    correctResponse: 'red',
+  }
+];
 
 /**
  * 
