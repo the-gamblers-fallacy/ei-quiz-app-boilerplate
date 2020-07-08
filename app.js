93c2f6a329f728fb5876e74b608217453917925a
Lucas Bigelow <lukejbigelow@gmail.com>
2020-07-08 14:56:09 -0500

    added questions array with five questions

--- app.js
+++ app.js
@@ -32,6 +32,34 @@ const store = {
   score: 0
 };
 
+const questions = [
+  {
+    question: 'How many planets are there?',
+    responses: ['6', '7', '8', '9'],
+    correctResponse: '8'
+  },
+  {
+    question: 'What constellation features three stars forming something of a belt?',
+    responses: ['Orion', 'Ursa Major', 'The Seven Sisters', 'The Moon'],
+    correctResponse: 'Orion'
+  },
+  {
+    question: 'How many times more massive is Saturn than Earth?',
+    responses: ['83', '213', '95', '113'],
+    correctResponse: '95'
+  },
+  {
+    question: 'How long does it take the Earth to revolve around the sun?',
+    responses: ['430 days', '234 days', '89 hours', '365 days'],
+    correctResponse: '365 days'
+  },
+  {
+    question: 'What is the official name of our sun?',
+    responses: ['Bob', 'Sol', 'The Sun', 'Icarus'],
+    correctResponse: 'Sol'
+  }
+];
+
 /**
  * 
  * Technical requirements:
@@ -58,5 +86,3 @@ const store = {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
-
-"This is Justin's attempt at submitting changes."
